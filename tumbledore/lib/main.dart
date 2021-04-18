25298b95272a3d72b47c001e9d9b8a8e60036d5a
Shruti Pistolwala <shruti.pistolwala@tum.de>
2021-04-18 12:54:51 +0200

    initial commit

--- tumbledore/lib/main.dart
+++ tumbledore/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
@@ -17,17 +16,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     /// Global widget for our app
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IsarPDX',
+
       debugShowCheckedModeBanner: false,
 
       /// Our custom theme colors
       theme: ThemeData(
-        primaryColor: Colors.orange.shade300,
-        scaffoldBackgroundColor: Colors.grey.shade800,
+        primaryColor: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
       ),
 
       /// The main page
-      home: MyHomePage(title: 'AIbus TUMbledore'),
+      home: MyHomePage(title: 'IsarPDX'),
     );
   }
 }
@@ -85,6 +85,68 @@ class _MyHomePageState extends State<MyHomePage> {
         "Human: $message\n"
         "AI:";
 
+//     ///shruti code
+// // This is the API request for Semantic Search
+
+// // This variable will hold the sections of the documentation.
+// // In this example the text in the textarea above will be imported and
+// // divided into an array using the "###" tag as the split indicator.
+// var documents;
+
+// // This variable will hold the scores for each section returned by the Semantic Search API
+// var scores;
+// var inputQuestion;
+
+// function apiScore(){
+
+//     // This splits the text in the textarea into a list of text blocks and removes surrounding white space
+//     documents = inputDocuments.value.split("###").map(Function.prototype.call, String.prototype.trim);
+
+//     // Clear the textarea
+//     inputDocuments.value = "";
+
+//     // The engine to use for Semantic Search
+//     var selectedEngine = "babbage";
+
+//     // The information we're sending to the API: An array of documents and the question as the query
+//     var _data = {
+//                     "documents": documents,
+//                     "query": inputQuestion.value
+//                 };
+
+//     // The header for the request
+//     var _headers = {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//         'Authorization': `Bearer ${apiKeyInput.value}`
+//     };
+
+//     // Our jquery request
+//     $.ajax({
+//     type:'POST',
+//     url: `https://api.openai.com/v1/engines/${selectedEngine}/search`,
+//     dataType:'JSON',
+//     headers: _headers,
+//     data: JSON.stringify(_data),
+//         success:function(result){
+
+//             // Assigning the result data to the scores variable to be used later
+//             scores = result.data;
+
+//             // Displaying the results in the document textarea
+//             result.data.forEach(element => {
+//                 var _doc = documents[element.document];
+//                 var _score = "Document: " + element.document + "\nScore: " + element.score + "\n----------------\n" + _doc + "\n\n";
+//                 inputDocuments.value += _score;
+//             });
+
+//         }
+//     });
+
+//     }
+
+// }
+
     /// Make the api request to OpenAI
     /// See available api parameters here: https://beta.openai.com/docs/api-reference/completions/create
     var result = await http.post(
@@ -97,7 +159,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: jsonEncode({
         "prompt": prompt,
         "max_tokens": 100,
-        "temperature": 0,
+        "temperature": 0.7,
         "top_p": 1,
         "stop": "\n",
       }),
@@ -121,6 +183,15 @@ class _MyHomePageState extends State<MyHomePage> {
       /// The top app bar with title
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.call,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
       ),
       body: Column(
         children: [
@@ -128,13 +199,19 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: ListView(
               padding: const EdgeInsets.all(20.0),
-              reverse: true, // makes it 'stick' to the bottom when sending new messages
+              reverse:
+                  true, // makes it 'stick' to the bottom when sending new messages
               children: messages.reversed.map((message) {
                 return Bubble(
                   child: Text(message.text),
-                  color: message.byMe ? Colors.orange.shade300 : Colors.grey.shade300,
-                  nip: message.byMe ? BubbleNip.rightBottom : BubbleNip.leftBottom,
-                  alignment: message.byMe ? Alignment.topRight : Alignment.topLeft,
+                  color: message.byMe
+                      ? Colors.orange.shade300
+                      : Colors.grey.shade300,
+                  nip: message.byMe
+                      ? BubbleNip.rightBottom
+                      : BubbleNip.leftBottom,
+                  alignment:
+                      message.byMe ? Alignment.topRight : Alignment.topLeft,
                   margin: BubbleEdges.symmetric(vertical: 5),
                 );
               }).toList(),
